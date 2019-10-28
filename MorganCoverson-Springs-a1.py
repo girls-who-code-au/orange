4c1c9118c5ae0356016fb1531a64a925f3e1e159
GWC <gwc@cyberlab.augusta.edu>
2019-10-28 19:49:50 -0400

    Added Morgan Coverson-Springs A1 submission.

---
+++ MorganCoverson-Springs-a1.py
@@ -0,0 +1,13 @@
+M="skittles"
+A="reeses"
+E="peppermint"
+
+timeofday = input("Enter the time of day (M,A,E): ")
+
+if (timeofday == "M"):
+    print(M)
+elif (timeofday == "A"):
+    print(A)
+else:
+    print(E)
+           
